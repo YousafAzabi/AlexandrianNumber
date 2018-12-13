076da61e767cfaf705999575e6636400b7a5fdfc
YousafAzabi <joeazabi@gmail.com>
2018-12-13 08:55:02 +0000

    reduced the code lines

--- src/AlexandrianNumber.java
+++ src/AlexandrianNumber.java
@@ -9,24 +9,16 @@ class AlexandrianNumber {
   }
 
   public ArrayList<Integer> find(int n) {
-    boolean tag = true;
     int p = 1, count = 1, x, result;
     while (count < n) {
       x = p * p + 1;
       for(int d = 2; d <= x ; d++) {
         if ( (x % d) == 0) {
           result = (int) (p * (p + d) * (p + (Math.pow(p,2) + 1) / d));
-          for(int item : list) {
-            if (result == item) {
-              tag = false;
-              break;
-            }
-          }
-          if (tag) {
+          if (!list.contains(result)) {
             list.add(result);
             count++;
           }
-          tag = true;
         }
       }
       p++;
