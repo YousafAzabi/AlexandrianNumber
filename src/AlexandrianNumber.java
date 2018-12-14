4ef7b484725eebd2368d0692abe2a02d46b47321
YousafAzabi <joeazabi@gmail.com>
2018-12-14 07:45:11 +0000

    Reduced loop time by looping through sspecific numbers

--- src/AlexandrianNumber.java
+++ src/AlexandrianNumber.java
@@ -1,28 +1,37 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 class AlexandrianNumber {
 
-  private ArrayList<Integer> list;
+  private ArrayList<Double> list;
+  private int[] divisorBasic = {1, 2, 5, 10, 13, 17};
 
   AlexandrianNumber() {
     list = new ArrayList();
   }
 
-  public ArrayList<Integer> find(int n) {
-    int p = 1, count = 1, x, result;
-    while (count < n) {
+  public ArrayList<Double> find(int n) {
+    int p = 1, count = 1;
+    double result, d, x;
+    while (count <= n) {
       x = p * p + 1;
-      for(int d = 2; d <= x ; d++) {
+      d = 1;
+      int i = 0;
+      while (d <= p) {
         if ( (x % d) == 0) {
-          result = (int) (p * (p + d) * (p + (Math.pow(p,2) + 1) / d));
+          result = (double) ( p * (p + d) * (p + ((p * p) + 1) / d) );
           if (!list.contains(result)) {
             list.add(result);
             count++;
+            //if (count%10000 == 0)System.out.println(p + " " + d + " " + count + " " + result + " " + i);
           }
         }
+        i++;
+        d = divisorBasic[i%6] + (24 * (int)(i/6) );
       }
       p++;
     }
+    Collections.sort(list);
     return list;
   }
 
@@ -33,9 +42,9 @@ class AlexandrianNumber {
   }
 
   public static void main(String[] args) {
-    int[] input = {7, 3, 4, 5};
+    int[] input = {1,2};
     AlexandrianNumber test = new AlexandrianNumber();
-    test.find(10);
+    test.find(1000);
     test.print(input);
   }
 
